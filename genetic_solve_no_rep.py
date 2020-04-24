e3b7f454407f5d141154364ceb244d4916a54952
Noam Stolero <stellern@gmail.com>
2020-04-24 16:33:32 +0300

    Finished genetic solving without reptitions
    Signed-off-by: Noam Stolero <stellern@gmail.com>

--- genetic_solve_no_rep.py
+++ genetic_solve_no_rep.py
@@ -1,12 +1,13 @@
 import random
 import numpy as np
 import itertools as it
+import copy
 
 POPULATION_SIZE = 1000
 TENTHS = int(POPULATION_SIZE / 10)
 OFFSPRINGS_MUL = 5
 NUM_OF_SURVIVORS = int(POPULATION_SIZE / OFFSPRINGS_MUL)
-MUTATION_CHANCE = 0.5
+MUTATION_CHANCE = 0.4
 RECT_VECTOR_OFFSET = 1
 POP_MAT_SHAPE = (POPULATION_SIZE, 16)
 
@@ -22,12 +23,17 @@ def create_population(shape: tuple):
 
 def mutation(pop_mat: np.ndarray):
     rng = np.random.default_rng()
-    probability_mat = np.random.random(pop_mat.shape)
+    probability_mat = rng.random(pop_mat.shape)
     probability_mat[0:NUM_OF_SURVIVORS, :] = 1  # Keeping parents untouched
-    prob_mat_01 = np.nonzero(probability_mat < MUTATION_CHANCE)
-    swap_prob_mat_01 = prob_mat_01.copy()
-    np.apply_along_axis(rng.shuffle, 1, swap_prob_mat_01)
-    pop_mat[prob_mat_01] = pop_mat[swap_prob_mat_01]
+    probability_mat[:, :2] = 1  # Keeping Fitness and 'A' Columns untouched
+    cell_to_swap_mat = probability_mat < MUTATION_CHANCE
+    rows_swap_indices_mat = [np.nonzero(row)[0] for row in cell_to_swap_mat[:,]]
+    shuffle_rows_swap_indices_mat = copy.deepcopy(rows_swap_indices_mat)
+    for i, row in enumerate(
+        shuffle_rows_swap_indices_mat[NUM_OF_SURVIVORS:], NUM_OF_SURVIVORS
+    ):
+        rng.shuffle(row)
+        pop_mat[i, rows_swap_indices_mat[i]] = pop_mat[i, row]
 
 
 def procreate_mat(pop_mat: np.ndarray):
@@ -72,6 +78,7 @@ def natural_selection():
     coef_mat = read_mat()
     pop_mat = create_population(POP_MAT_SHAPE)
     i = 0
+    no_improve_counter = 0
     best_fit = float("inf")
     while True:
         # pop_mat[:, RECT_VECTOR_OFFSET] = 8
@@ -80,20 +87,27 @@ def natural_selection():
         min_fit = pop_mat[arg_min_fit, 0]
         best_sol = pop_mat[arg_min_fit, RECT_VECTOR_OFFSET:]
         if min_fit < best_fit or not i % 200:
+            no_improve_counter = 0
             print(
                 "Best of generation {i}:\nFitness:{fit:.4f}, Solution Vector: {vect}".format(
                     i=i, fit=min_fit, vect=best_sol
                 )
             )
             best_fit = min_fit
+        else:
+            no_improve_counter += 1
+
+        if no_improve_counter >= 100:
+            pop_mat = create_population(POP_MAT_SHAPE)
+            i = 0
+            continue
+
         if min_fit == 0:
             break
 
         pop_mat = keep_only_parents(pop_mat)
         pop_mat = procreate_mat(pop_mat)
         mutation(pop_mat)
-        # if i == 1000:
-        # pop_mat = create_population(POP_MAT_SHAPE)
         i += 1
 
 
